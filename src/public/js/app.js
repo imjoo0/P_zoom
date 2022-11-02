a15cf64eee50c987a8f49b2c73001c6343d82994
imjoo0 <im_joo0@naver.com>
2022-11-02 14:18:44 +0900

    room alert

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,65 @@
-const socket = io();
+const socket = io();const socket = io(); // io function은 알아서 socket.io를 실행하고 있는 서버를 찾을 것 
+
+// 방을 만들 것 ( socketio에는 이미 방 기능이 있음 ) 
+
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+const room = document.getElementById("room");
+
+room.hidden = true; // 처음에는 방안에서 할 수 있는 것들을 안보이게 
+
+let roomName;
+
+function addMessage(message){
+    const ul = room.querySelector("ul");
+    const li = document.createElement("li");
+    li.innerText = message;
+    ul.appendChild(li);
+}
+
+function handleMessageSubmit(event){
+    event.preventDefault();
+    const input = room.querySelector("#msg input");
+    const value = input.value;
+    socket.emit("new_message",input.value,roomName,()=>{
+        addMessage(`You:${value}`);
+    }); // 백엔드로 new_message 이벤트를 날림, input.value랑 방 이름도 같이 보냄. 
+    // 마지막 요소는 백엔드에서 시작시킬 수 있는 함수 
+    input.value = "";
+}
+function showRoom(){ // 방에 들어가면 방 내용이 보이게 
+    welcome.hidden = true;
+    room.hidden = false;
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room ${roomName}`; // 저장된 방 이름을 pug의 요소에 전달해서 띄운다. 
+    const msgForm = room.querySelector("#msg");
+    msgForm.addEventListener("submit",handleMessageSubmit);
+}
+
+function handleRoomSubmit(event){
+    event.preventDefault();
+    const input = form.querySelector("input");
+		// argument 보내기 가능 ( socketIO는 Object 전달 가능 ) 
+		// 첫번째는 이벤트명( 아무거나 상관 없음 ) , 두번째는 front-end 에서 전송하는 object( 보내고 싶은 payload), 세번째는 서버에서 호출하는 function 
+    socket.emit( // emit의 마지막 요소가 function이면 가능
+			"enter_room",
+			input.value,
+			showRoom //백엔드에서 끝났다는 사실을 알리기 위해 function을 넣고 싶다면 맨 마지막에 넣자 
+		); // 1. socketIO를 이용하면 모든 것이 메시지 일 필요가 없다 
+			 // 2. client는 어떠한 이벤트든 모두 emit 가능 / 아무거나 전송할 수 있다. ( text가 아니어도 되고 여러개 전송이 가능하다 ) 
+    roomName = input.value; // roomName에 입력한 방 이름 저장 
+    input.value = "";
+}
+
+// 서버는 백엔드에서 function을 호출하지만 function은 front-end에서 실행 됨 
+form.addEventListener("submit",handleRoomSubmit);
+
+socket.on("welcome", (user)=>{
+    addMessage("${user} arrived!");
+});
+
+socket.on("bye", (left)=>{
+    addMessage("${left} left! 😂");
+});
+
+socket.on("new_message",addMessage); // addMessage만 써도 알아서 msg를 매개 변수로 넣는다. 
--- src/server.js
+++ src/server.js
@@ -1,18 +1,42 @@
-import http from "http";
+import http from "http"; // 기본 설치 
 import SocketIO from "socket.io";
-import express from "express";
+import express from "express"; // npm i express
 
-const app = express();
-app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (_, res) => res.render("home"));
-app.get("/*", (_, res) => res.redirect("/"));
+const app = express(); // app 변수에 가져와서 사용 
 
-const httpServer = http.createServer(app);
-const wsServer = SocketIO(httpServer);
-
-wsServer.on("connection", (socket) => {
-  console.log(socket);
-});
+app.set("view engine","pug"); // 뷰엔진을 pug로 설정 
+app.set("views",__dirname+"/views"); // 디렉토리 설정 
+app.use("/public", express.static(__dirname + "/public")); // public 폴더를 유저에게 공개 ( 유저가 볼 수 있는 폴더 지정 )
+app.get("/", (_, res) => res.render("home")); // 홈페이지로 이동할 때 사용될 템플릿을 렌더 
+app.get("/*", (_, res) => res.redirect("/")); // 홈페이지 내 어느 페이지에 접근해도 홈으로 연결되도록 리다이렉트 ( 다른 url 사용 안 할 거라 ) 
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
+
+const httpServer = http.createServer(app); // app은 requestlistener 경로 - express application 으로 부터 서버 생성 
+const wsServer = SocketIO(httpServer); // localhost:3000/socket.io/socket.io.js로 연결 가능 (socketIO는 websocket의 부가기능이 아니다!!)
+
+//websocket에 비해 개선 1 . 어떤 이벤트 든지 전달이 가능 하다. 2. JSobject를 보낼 수 있다. 
+wsServer.on("connection", (socket) => {
+    socket["nickname"] = "Anonymous";
+    
+    socket.onAny( (event)=>{ // 미들웨어 같은 존재 , 어느 이벤트에서든지 console.log 할 수 있음. 
+        console.log(`Socket Event: ${event}`);
+    });
+
+    socket.on("enter_room",(roomName, done) => { // 여기 있는 done함수는 여기서 실행하지 않는다 - 사용자로부터 함수를 받아와서 사용하면 보안문제가 생길 수 있기 때문에 
+        socket.join(roomName); // 현재 들어가 있는 방 표시 ( 기본적으로 User와 Server 사이에 private rooom이 있다. )
+        done();
+        socket.to(roomName).emit("welcome",socket.nickname); // welcome 이벤트를 roomName에 있는 모든 사람들에게 emit 한 것 
+  });
+
+  socket.on("disconnecting", ()=>{ // 클라이언트가 서버와 연결이 끊어지기 전에 마지막 굿바이 메시지를 보낼 수 있다. 
+    socket.rooms.forEach( (room) => socket.to(room).emit("bye",socket.nickname)); // 방안에 있는 모두에게 보내기 위해 forEach 
+  });
+
+  socket.on("new_message",(msg,room,done)=>{ // 메시지랑 done함수를 받을 것 
+    socket.to(room).emit("new_message",msg); // new_message 이벤트를 emit 한다. 방금 받은 메시지가 payload 된다. 
+    done();// done은 프론트엔드에서 코드를 실행할 것!! (백엔드에서 작업 다 끝나고!!)
+  });
+});
+
 httpServer.listen(3000, handleListen);
